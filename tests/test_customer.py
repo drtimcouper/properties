81c7eacf2a54511ee9849bf614730a07ea4805cf
Tim Couper <drtimcouper@gmail.com>
2016-04-09 13:31:38 +0100

    better_customer tests

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -1,8 +1,20 @@
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_raises
 
 import properties.customer as customer
 
 
-def test_customer():
-    a = customer.Customer('Fred')
-    assert_equal(a.orders, customer.ORDERS)
+def test_customer_orders():
+    c = customer.Customer('Fred')
+    assert_equal(c.orders, customer.ORDERS)
+
+
+def test_better_customer_orders():
+    c = customer.BetterCustomer('Fred')
+    assert_equal(c.orders, customer.ORDERS)
+
+
+def test_better_customer_orders_readonly():
+     c = customer.BetterCustomer('Fred')
+     with assert_raises(AttributeError) as err:
+        c.orders = 'stuff'
+     assert_equal(str(err.exception), "can't set attribute" )
